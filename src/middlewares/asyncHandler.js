adcf116689fe0fbb5d71b88ffb7f004298def6f5
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-26 09:52:47 +0530

    refactor: simplify asyncHandler middleware implementation

--- src/middlewares/asyncHandler.js
+++ src/middlewares/asyncHandler.js
@@ -2,8 +2,6 @@
  * Async handler middleware to wrap async route handlers
  * Catches errors and passes them to error handling middleware
  */
-const asyncHandler = (fn) => (req, res, next) => {
-  return Promise.resolve(fn(req, res, next)).catch(next);
-};
+const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
 module.exports = asyncHandler;
